1db2c31a3994919cfe47e43b1ac42e354ba84dd0
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-09 19:51:07 +0530

    Q

--- app.js
+++ app.js
@@ -13,12 +13,22 @@
 
 // console.log(greed("",21))
 
-function swapNumbers(a, b) {
-    // Write your logic here
-    let c = a + b;
-    a = c - a;
-    b = c - b;
-    return console.log(a , b)
+// function swapNumbers(a, b) {
+//     // Write your logic here
+//     let c = a + b;
+//     a = c - a;
+//     b = c - b;
+//     return console.log(a , b)
     
-}
-swapNumbers(5,10)
+// }
+// swapNumbers(5,10)
+
+// let i = 11;
+// i = i++ + ++i
+// console.log(i)
+
+let a = 11 ,b=22,c;
+c=a+b+a++ + b++ + ++a+ ++b;
+console.log('a :',a)
+console.log('b :',b)
+console.log('c :',c)
