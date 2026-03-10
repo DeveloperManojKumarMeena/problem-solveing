481f2df7681738625691b55d1440eb3a732147fb
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-10 08:10:17 +0530

    sheet 27 done

--- app.js
+++ app.js
@@ -36,3 +36,35 @@
 // let b = true;
 // b++
 // console.log(b)
+
+// let i =1,j=2,k=3;
+// let m = i-- - j-- - k--;
+// console.log('i :',i)
+// console.log('j :',j)
+// console.log('k :',k)
+// console.log('m :',m)
+
+// let a = 1 , b = 2;
+// console.log(--b- ++a + ++b - --a)
+
+// let i = 19 , j = 29, k;
+// k = i-- -i++ + --j - ++j + --i - j-- + ++i - j++
+// console.log(k)
+
+// let i = 11;
+// let j = --(i++);
+// console.log(j)
+
+// let m = 0 , n = 0 ;
+// let p = --m*--n*n--*m--;
+// console.log(p)
+
+// let a = 1 ;
+// a = a++ + ++a * --a - a--;
+// console.log(a)
+
+// let a = 11++
+// console.log(a)
+
+let i = 0 ,j=0;
+console.log(--i*i++ * ++j * j++)
