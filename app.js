53b0504c315d0d601fdd8fc14ab30733318c9c99
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-10 07:33:14 +0530

    increment and decrement operator examples commented out in app.js

--- app.js
+++ app.js
@@ -27,8 +27,12 @@
 // i = i++ + ++i
 // console.log(i)
 
-let a = 11 ,b=22,c;
-c=a+b+a++ + b++ + ++a+ ++b;
-console.log('a :',a)
-console.log('b :',b)
-console.log('c :',c)
+// let a = 11 ,b=22,c;
+// c=a+b+a++ + b++ + ++a+ ++b;
+// console.log('a :',a)
+// console.log('b :',b)
+// console.log('c :',c)
+
+// let b = true;
+// b++
+// console.log(b)
